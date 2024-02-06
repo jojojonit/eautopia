e6b396bfc3ae9b1c34c9e283f4b398da9daf0db0
Jonit <jonitowt@gmail.com>
2024-02-06 22:32:03 +0800

    added sign up form

---
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,113 @@
+import { Button, Form, Input } from "antd";
+
+export default function SignUpForm() {
+  const handleSubmit = (values) => {
+    console.log("Received values of form: ", values);
+  };
+
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 8 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 16 },
+    },
+  };
+
+  const tailFormItemLayout = {
+    wrapperCol: {
+      xs: {
+        span: 24,
+        offset: 0,
+      },
+      sm: {
+        span: 16,
+        offset: 8,
+      },
+    },
+  };
+
+  return (
+    <>
+      <Form
+        {...formItemLayout}
+        name="sign up"
+        onFinish={handleSubmit}
+        style={{ maxWidth: 600 }}
+      >
+        <Form.Item
+          name="name"
+          label="Name"
+          tooltip="What do you want others to call you?"
+          rules={[
+            {
+              required: true,
+              message: "Please input your name",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            { type: "email", message: "Please enter a valid email" },
+            {
+              required: true,
+              message: "Please input your E-mail!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          name="password"
+          label="Password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+          hasFeedback
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          name="confirm"
+          label="Confirm Password"
+          dependencies={["password"]}
+          hasFeedback
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(
+                  new Error("The new password that you entered do not match!")
+                );
+              },
+            }),
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailFormItemLayout}>
+          <Button type="primary" htmlType="submit">
+            Sign Up
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+}
--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import AdminPage from "../AdminPage/AdminPage";
 import Homepage from "../Homepage/Homepage";
+import TestPage from "../Test/Test";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/test" element={<TestPage />} />
       </Routes>
     </>
   );
