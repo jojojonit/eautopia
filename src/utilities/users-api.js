b205268d7526f099d2ed3ae50b84920cd5e8486e
Jonit <jonitowt@gmail.com>
2024-02-07 03:05:09 +0800

    signup feature working

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,21 +1,20 @@
 // This is the base path of the Express route we'll define
-const BASE_URL = "/api/users";
+const BASE_URL = "/api/user";
 
 export async function signUp(userData) {
-  // Fetch uses an options object as a second arg to make requests
-  // other than basic GET requests, include data, headers, etc.
-  const res = await fetch(BASE_URL, {
+  console.log("Request data:", userData);
+
+  const res = await fetch(BASE_URL + "/create", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    // Fetch requires data payloads to be stringified
-    // and assigned to a body property on the options object
     body: JSON.stringify(userData),
   });
-  // Check if request was successful
+  console.log("Response from backend:", res);
   if (res.ok) {
-    // res.json() will resolve to the JWT
+    console.log("Return JSON");
     return res.json();
   } else {
+    console.log("Error in signup");
     throw new Error("Invalid Sign Up");
   }
 }
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -1,9 +1,31 @@
 import * as usersAPI from "./users-api";
 
 export async function signUp(userData) {
-  // Delegate the network request code to the users-api.js API module
-  // which will ultimately return a JSON Web Token (JWT)
-  const token = await usersAPI.signUp(userData);
-  // Baby step by returning whatever is sent back by the server
+  const res = await usersAPI.signUp(userData);
+  const token = res.token;
+  if (!res) {
+    return null;
+  }
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem("token");
+    return null;
+  }
+  localStorage.setItem("token", token);
+  return res.user;
+}
+export function getToken() {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    return null;
+  }
+  // Obtain the payload of the token
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  if (payload.exp < Date.now() / 1000) {
+    // Token has expired - remove it from localStorage
+    localStorage.removeItem("token");
+    return null;
+  }
   return token;
 }
