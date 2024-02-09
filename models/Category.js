cab62bac67ac3818be45be58bbed3a2bd93771a3
Jonit <jonitowt@gmail.com>
2024-02-10 02:20:23 +0800

    added category and review models

---
+++ models/Category.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require("mongoose");
+
+const categorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Category", categorySchema);
---
+++ models/Review.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const reviewSchema = new Schema(
+  {
+    title: {
+      type: String,
+    },
+    body: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    },
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Review", reviewSchema);
