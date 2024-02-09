f068b3361821409d5bf5f0ac62a0e149865094c8
Jonit <jonitowt@gmail.com>
2024-02-09 13:11:17 +0800

    added functions to update product on server

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,7 +19,33 @@ const create = async (req, res) => {
   }
 };
 
+const update = async (req, res) => {
+  const { id } = req.params;
+  const newData = req.body;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ error: "PRODUCT not found" });
+    }
+    // product.name = newData.name;
+
+    Object.keys(newData).forEach((key) => {
+      if (product[key] !== undefined) {
+        product[key] = newData[key];
+      }
+    });
+
+    await product.save();
+    res
+      .status(200)
+      .json({ message: "Product UPDATED successfully", product, id, newData });
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getAll,
   create,
+  update,
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -4,5 +4,6 @@ const productController = require("../controllers/productController");
 
 router.get("/", productController.getAll);
 router.post("/create", productController.create);
+router.patch("/:id/update", productController.update);
 
 module.exports = router;
